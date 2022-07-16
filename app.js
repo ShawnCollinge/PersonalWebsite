fbcfb795fb5559dc81faf0e0ad983eb9cb51764c
Shawn Collinge <shawncollinge@Shawns-MacBook-Pro.local>
2022-07-16 13:02:09 -0700

    add uploader and backend project adder

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const multer  = require('multer')
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+
 app.use(express.static("public"));
+app.use('/uploads', express.static('uploads'))
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -26,31 +38,130 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const about = {
-  title: "Welcome to my personal website",
-  body: "I am a computer science student attending Shoreline Community college. This is my website to show off projects I've worked on and explain a little about myself"
-};
-
-
-mongoose.connect("mongodb://localhost:27017/userDB");
+mongoose.connect(process.env.MONGO_URL);
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
 
+const projectSchema = new mongoose.Schema({
+  title: String,
+  githubLink: String,
+  techStacks: String,
+  demoLink: String,
+  description: String,
+  images: Array,
+  isFeatured: Boolean
+});
+
 userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+const Project = new mongoose.model("Project", projectSchema);
 
 passport.use(User.createStrategy());
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+const aboutContent = {
+  title: "test",
+  body: "test"
+}
+
 app.get("/", function(req, res) {
-  res.render("home", {about: about});
+  Project.find({isFeatured: true}, function(err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+        res.render("home", {featureProjects: posts, about: aboutContent});
+    }
+  })
 });
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/logout", function(req, res) {
+  req.logout(function(err) {
+    if (err) {
+      console.log(err) }});
+  res.redirect("/");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.get("/admin", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("admin");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/admin/newproject", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("newProject");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/admin/newproject", upload.array('images', 12), function(req, res) {
+  if (req.isAuthenticated()) {
+    const project = new Project({
+      title: req.body.title,
+      githubLink: req.body.githubLink,
+      techStacks: req.body.techStacks,
+      demoLink: req.body.demoLink,
+      description: req.body.description,
+      images: req.files,
+      isFeatured: req.body.isFeatured
+    });
+    project.save();
+    res.render("newProject");
+  } else {
+    res.redirect("/login");
+  }
+})
+
+// app.get("/admin/edit/:id", function(req, res) {
+//   console.log("Asdf");
+// });
+app.post("/register", function(req, res) {
+
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/admin");
+      });
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/admin");
+      });
+    }
+  });
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
