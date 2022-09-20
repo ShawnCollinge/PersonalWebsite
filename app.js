e84b70c997eac1d931a3d28b360c1f2701913f1a
Shawn Collinge <shawncollinge@gmail.com>
2022-09-20 02:04:52 +0000

    Remove debugging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const createError = require('http-errors');
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -58,7 +59,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = {} // req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const user_controller = require("../controllers/usersController.js");
 
-//router.get("/register", user_controller.register_page);
-//router.post("/register", user_controller.register_post);
+router.get("/register", user_controller.register_page);
+router.post("/register", user_controller.register_post);
 
 router.get("/login", user_controller.login_page);
 router.get("/logout", user_controller.logout);
