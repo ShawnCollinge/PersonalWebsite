3b28254c87e765c3bcee1d344d527d71f86e06b5
Shawn Collinge <shawncollinge@gmail.com>
2022-12-26 20:30:33 +0000

    Fix login

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const user_controller = require("../controllers/usersController.js");
+const passport = require("passport");
+
 
 router.get("/register", user_controller.register_page);
 router.post("/register", user_controller.register_post);
 
-router.get("/login",    "/login",
+router.get("/login", 
 passport.authenticate('local', {
     failureRedirect: '/login',
     successRedirect: '/',
