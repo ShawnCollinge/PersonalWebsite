0d645971f8c312548a637646d40fc69b22dfa34e
Shawn Collinge <shawnc6@cs.washington.edu>
2023-12-18 17:16:36 -0800

    Updated darkmode

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -8,6 +8,16 @@
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
   }
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 
   // Function to get a cookie
 
@@ -34,7 +44,7 @@
         icon.classList.add('fa-moon');
       }
     }
-    
+
     // Apply dark mode based on cookie
     if (currentMode === 'enabled') {
       document.body.classList.add('dark-mode');
