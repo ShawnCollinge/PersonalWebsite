b27b5c21221bdbd6749ec9bc9711335c0f02a1e4
Shawn Collinge <shawnc6@cs.washington.edu>
2023-12-18 17:15:41 -0800

    Updated darkmode

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -10,16 +10,6 @@
   }
 
   // Function to get a cookie
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
 
   document.addEventListener('DOMContentLoaded', function() {
     const toggle = document.getElementById('dark-mode-toggle');
@@ -44,13 +34,7 @@
         icon.classList.add('fa-moon');
       }
     }
-
-    if (!currentMode) {
-      setCookie('darkMode', 'enabled', 7);
-      currentMode = 'enabled';
-      document.body.classList.add('dark-mode');
-    }
-  
+    
     // Apply dark mode based on cookie
     if (currentMode === 'enabled') {
       document.body.classList.add('dark-mode');
---
+++ public/js/darkmode2.js
@@ -0,0 +1,17 @@
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentMode = getCookie('darkMode');
+    if (!currentMode) {
+        setCookie('darkMode', 'enabled', 7);
+    }
+});
